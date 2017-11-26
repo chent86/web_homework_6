b3bdd760028f796f779bc2d3e1abdf41b14d6f2a
Liangpin_Hou <bz20100405@163.com>
2017-11-26 22:13:49 +0800

    update mole partly
    72-70
    ????
    I just make it short, but still so much lines...

--- mole/mole.js
+++ mole/mole.js
@@ -2,15 +2,13 @@
 var begin = 0;
 
 window.onload = function() {
-	var get_div = document.createElement("div");
-	var ground=document.getElementById("ground");            //画地鼠洞
-	for(var i = 0; i < 60; i++) {
+	_.times(60, function(){
 		var tmp = document.createElement("div");
-		tmp.addEventListener('click', find);
+		tmp.addEventListener('click', find);              //画地鼠洞
 		tmp.className = "hole";
-		ground.append(tmp);
-	}
-	document.getElementById('record').children[0].addEventListener('click', start_or_stop);
+		$("#ground").append(tmp);
+	}); 
+	$("#go").bind('click', start_or_stop);
 }
 
 function start_or_stop() {
@@ -22,27 +20,27 @@ function start_or_stop() {
 
 function start() {
 	begin = 1;
-	document.getElementsByTagName("input")[2].value = "0";
-	document.getElementById('mode').value = "Playing";
-	document.getElementsByTagName("input")[0].value = "31";
+	$("input")[2].value = "0";
+	$("#mode")[0].value = "Playing";
+	$("input")[0].value = "31";
 	time();
 	lift();
 }
 
 function end() {
-	document.getElementById('mode').value = "Game Over";
+	$("#mode")[0].value = "Game Over";
 	begin = 0;
 	for(var i = 0; i < 60; i++)
-		if(document.getElementById('ground').children[i].className == "mouse") {
-			document.getElementById('ground').children[i].className = "hole"      //清理留下的蓝色			
+		if($("#ground")[0].children[i].className == "mouse") {
+			$("#ground")[0].children[i].className = "hole"      //清理留下的蓝色			
 		}
-	alert("Game Over\nYou score is: "+document.getElementsByTagName("input")[2].value);
+	alert("Game Over\nYou score is: "+$("input")[2].value);
 }
 
 function time() {
 	if(begin == 0)
 		return;
-	var current = document.getElementsByTagName("input")[0];
+	var current = $("input")[0];
 	if(parseInt(current.value) > 0)
 		current.value = (parseInt(current.value)-1).toString();
 	if(parseInt(current.value) <= 0) {
@@ -55,18 +53,18 @@ function time() {
 
 function find() {
 	if(begin == 1) {
-		var tmp = document.getElementsByTagName("input")[2].value;
+		var tmp = $("input")[2].value;
 		if(this.className == "mouse") {
-			document.getElementsByTagName("input")[2].value = (parseInt(tmp)+1).toString();
+			$("input")[2].value = (parseInt(tmp)+1).toString();
 			this.className = "hole";
 			lift();
 		}
 		else
-			document.getElementsByTagName("input")[2].value = (parseInt(tmp)-1).toString();
+			$("input")[2].value = (parseInt(tmp)-1).toString();
 	}
 }
 
 function lift() {
-	var ran = document.getElementById("ground").children[Math.round(Math.random()*59)];
+	var ran = $("#ground")[0].children[_.random(59)];
 	ran.className = "mouse";
 }
